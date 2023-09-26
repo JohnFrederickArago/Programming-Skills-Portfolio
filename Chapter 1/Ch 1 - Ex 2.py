25a49f5a6356301b4f93dc26f1e37f2cbec4cef5
JohnFrederickArago <144773489+JohnFrederickArago@users.noreply.github.com>
2023-09-26 12:40:40 +0400

    Add files via upload

---
+++ Chapter 1/Ch 1 - Ex 2.py
@@ -0,0 +1,15 @@
+"""
+Chapter 1 - Exercise 2 : Print the Version of Python 
+
+Write a Python program to get the Python version you are using.
+"""
+
+import sys 
+print("Python version") 
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+"""
+I imported sys, then printed "Python version". Then I printed sys.version and
+sys.version_info to show the version of the python in my system.
+"""
---
+++ Chapter 1/Ch 1 - Ex 4.py
@@ -0,0 +1,12 @@
+
+"""
+Chapter 1 - Exercise 4: Strings Concatination
+
+Write three strings in different variables and print the output as one string.    
+"""
+
+a = "My name is"
+b = "John Frederick"
+c = "Arago" #I added variables with string values of my name
+
+print(a, b, c) #Then printed them all into one string
